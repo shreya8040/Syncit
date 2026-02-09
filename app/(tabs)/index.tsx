c37f5edb4bafd939e23b11f2f66f5b08a3301cf6
shreya <shreya8040@gmail.com>
2026-02-09 22:05:40 +0530

    making it exist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,61 @@
 import { ThemedText } from '@/components/themed-text';
-import { View, ImageBackground, StyleSheet, StatusBar, Button, Alert } from 'react-native';
+import { Text,View, ImageBackground, StyleSheet, StatusBar, Button, Alert } from 'react-native';
+import { Checkbox } from 'expo-checkbox';
 import { useThemeColor } from '@/hooks/use-theme-color';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { Colors } from '@/constants/theme';
+import { useState } from 'react';
 
-const image = {uri: 'https://i.pinimg.com/736x/a4/fd/14/a4fd14265f85fcdc3ec396472cb20282.jpg'};
+const image = {uri: 'https://i.ibb.co/27DM0ygH/Whats-App-Image-2026-02-09-at-8-19-25-PM.jpg'};
 
-
+ 
 export default function HomeScreen() {
+  const [isChecked, setChecked] = useState(false);
   return (
     <SafeAreaProvider>
-    <SafeAreaView style={{flex:1, width:'100%',height:'100%', alignItems:'center', backgroundColor:useThemeColor({},'background')}} edges={['left', 'right']}>
+    <SafeAreaView style={{flex:1,alignItems: 'center', width:'100%',height:'100%',  backgroundColor:useThemeColor({},'background')}} edges={['left', 'right']}>
       
-        <ImageBackground source={image} resizeMode='cover' style={{flex:0.1,  marginTop:35, marginLeft:35, alignItems:'center', width:'90%', height:'100%'}}></ImageBackground>
-        <Button
-        title="Click Me"
-        color="#ff5a68" // Platform-specific color behavior
-        accessibilityLabel="Tap this button to see an alert"
+        <ImageBackground source={image} resizeMode='cover' style={{flex:0.1,  marginTop:35, marginLeft:2, borderRadius:20 , overflow: 'hidden',  alignItems:'center', width:'100%', height:'100%'}}></ImageBackground>
+        <Text style={{fontSize:30, fontWeight:'bold', marginTop:20, color:useThemeColor({},'text')}}>Welcome to SyncIt!</Text>
+        <View style={styles.checkboxContainer}>
+          
+          <Checkbox
+          style={styles.checkboxes}
+          value={isChecked}
+          onValueChange={setChecked}
+          color={isChecked ? '#bb30fb' : undefined}
+        />
+        <Checkbox
+          style={styles.checkboxes}
+          value={isChecked}
+          onValueChange={setChecked}
+          color={isChecked ? '#bb30fb' : undefined}
+        />
+        <Checkbox
+          style={styles.checkboxes}
+          value={isChecked}
+          onValueChange={setChecked}
+          color={isChecked ? '#bb30fb' : undefined}
+        />
         
-      />
-        </SafeAreaView>
+        </View>
+             </SafeAreaView>
       
      </SafeAreaProvider>
   );
 }
+const styles = StyleSheet.create({
+  checkboxes:{
+    padding:10,
+    margin:8
+  },
+  checkboxContainer:{
+  flexDirection: 'row',
+  alignItems: 'center',
+ 
+  width: '100%',         
+  alignSelf: 'flex-start',
+  marginTop: 30,
+  paddingHorizontal: 20,
+  }
+})
