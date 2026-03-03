799dc8df3b17592b341c8c0f738be2c5db0610a6
shreya <shreya8040@gmail.com>
2026-03-03 21:56:09 +0530

    camera working from sdk

--- app/(tabs)/modal.tsx
+++ app/(tabs)/modal.tsx
@@ -1,18 +1,71 @@
-import { useThemeColor } from '@/hooks/use-theme-color';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
 
-export default function Modal() {
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <View style={styles.container}>
-     <Text style={{fontSize:30, fontWeight:'bold', marginTop:20, color:"black"}}>Modal Screen</Text>
+      <CameraView style={styles.camera} facing={facing} />
+      <View style={styles.buttonContainer}>
+        
+          
+        
+      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#1c1c1c',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 0.8,
+
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
---
+++ services/mlKitService.ts
@@ -0,0 +1,12 @@
+import TextRecognition from '@react-native-ml-kit/text-recognition';
+export const scannedText = async(imagePath:string) => {
+    try {
+        const result = await TextRecognition.recognize(imagePath);
+        console.log('Scanned Text:', result);
+        return result;
+    }
+    catch (error) {
+        console.error('Error scanning text: ', error);
+        throw error;
+    }
+}
